92c0a24958c43d17463acdc604a1ea0b5142edcc
RishabhChandrakar <162026054+RishabhChandrakar@users.noreply.github.com>
2024-04-09 13:46:13 +0530

    imageprocessing.py

---
+++ TASK_7/imageprocessing.py
@@ -0,0 +1,18 @@
+
+# import opencv 
+import cv2
+  
+path=r"C:\\Users\\chint\\AppData\\Local\\Programs\\Python\\Python310\\Summer Project Robotics Club Task\\Screenshot 2024-04-08 095210.png" 
+
+image = cv2.imread(path)
+cv2.imshow('Original', image)
+
+cv2.waitKey(0) 
+  
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+
+cv2.imshow('Grayscale',gray_image) 
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
